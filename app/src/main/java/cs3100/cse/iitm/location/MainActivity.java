29e1fe675814fd95e1d7b8756b437d3bc53e4e98
Gokulan <gokulan97@gmail.com>
2017-09-18 23:19:55 +0530

    initial commit

---
+++ app/src/main/java/cs3100/cse/iitm/location/MainActivity.java
@@ -0,0 +1,13 @@
+package cs3100.cse.iitm.location;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
